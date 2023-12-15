8b3ad6f72804324da6c0393d32ae7ab63ffec63b
zakheledyosba <zakheledyosiba94@gmail.com>
2023-12-15 01:11:40 -0800

    zakhele

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 TASKS
 1.write a simple shell programme
+2.Write a beautiful code that passes the Betty checks
+3.Write a UNIX command line interpreter.
+4.Handle command lines with arguments
+5.Handle the PATH
--- check_chain.c
+++ check_chain.c
@@ -2,9 +2,13 @@
 
 void check_chain(info_t *info, char *buf, size_t *k, size_t o, size_t len)
 {
-    printf("Checking chain from index %ld to %ld:\n", o, len - 1);
-    for (size_t i = o; i < len; ++i)
+
+	size_t i;
+	printf("Checking chain from index %lu to %lu:\n", o, len - 1);
+	for ( i = o; i < len; ++i)
 	{
+		*k = i;
+		printf("info value:%d\n", info ->readfd); 
 		putchar(buf[i]);
 	}
 
